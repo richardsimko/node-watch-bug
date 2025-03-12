2814663f31f9f591decfa47e7339d18156b0bb4d
Richard Simko <1245031+richardsimko@users.noreply.github.com>
2025-03-12 11:36:03 +0100

    Even smaller example

--- app-foobar/app-foobar.js
+++ app-foobar/app-foobar.js
@@ -1,8 +1,15 @@
-import { initCommonApp } from "../init/init-common.js";
+import { Server } from "http";
+import { test2 } from "../app/test.js";
 const port = process.env.PORT || "5002";
 
 async function init() {
-  await initCommonApp(port);
+  console.log(test2);
+  // Server just to keep the process alive
+  const server = new Server();
+  server.listen(port);
+  server.on("listening", () =>
+    console.info("Server listening on port " + port)
+  );
 }
 
 void init();
